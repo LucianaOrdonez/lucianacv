e8f40a95b8734d1b44ca94182d8b0dc415fee305
Luciana Ordoñez <ordonezz.088@gmail.com>
2023-03-01 12:48:47 -0300

    Primer escritura en el repositorio

---
+++ function.js
@@ -0,0 +1,4 @@
+document.getElementById('botón').onclick= function() {
+    console.log("Capturamos el evento click");
+    document.getElementById('envío').innerHTML = "¡Muchas gracias por su tiempo! en breve estaré en contacto";
+   }
